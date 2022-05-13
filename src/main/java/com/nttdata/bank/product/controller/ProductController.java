eea8454150ec7c63482cccfb8109687ad7e9aba9
Jesus Gian Marco Castro Morales <jcasmora@emeal.nttdata.com>
2022-05-13 11:07:17 -0500

    tercer commit product

--- src/main/java/com/nttdata/bank/product/controller/ProductController.java
+++ src/main/java/com/nttdata/bank/product/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.nttdata.bank.product.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nttdata.bank.product.model.Product;
@@ -13,7 +12,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/product")
 public class ProductController {
 	
 	@Autowired
